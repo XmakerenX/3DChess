60973bc77056676e6bdbfec0539e56287cb25323
Matan Keren <xmakerenx@gmail.com>
2018-05-15 11:23:47 +0300

    Added keyTables and mouseEvents enums
    keyTables are used to convert system native enums to our game enums.
    mouseEvents are used to convert mouse events to our enums.

---
+++ mouseEventsGame.h
@@ -0,0 +1,15 @@
+#ifndef  _MOUSEEVENTSGAME_H
+#define  _MOUSEEVENTSGAME_H
+
+enum class MouseEvent{Unknown = 0,
+		      LeftButton,
+		      DoubleLeftButton,
+		      MiddleButton,
+		      DoubleMiddleButton,
+		      RightButton,
+		      DoubleRightButton,
+		      ScrollHoriz,
+		      ScrollVert,
+		      MouseMoved};
+
+#endif  //_MOUSEEVENTSGAME_H
