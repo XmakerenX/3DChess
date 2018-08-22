5725d2209acec88c38220d74a9322a189fff7dec
Matan Keren <xmakerenx@gmail.com>
2018-08-22 14:41:47 +0300

    Fixed keys scan codes are have values offseted by 8

--- input.h
+++ input.h
@@ -2,22 +2,22 @@
 #define  _INPUT_H
 
 //Keys scancodes
-#define GK_UP    111
-#define GK_DOWN  116
-#define GK_LEFT  113
-#define GK_RIGHT 114
+#define GK_UP    103
+#define GK_DOWN  108
+#define GK_LEFT  105
+#define GK_RIGHT 106
 
-#define GK_A 38
-#define GK_S 39
-#define GK_D 40
-#define GK_W 25
+#define GK_A 30
+#define GK_S 31
+#define GK_D 32
+#define GK_W 17
 
-#define GK_Z 52
-#define GK_X 53
+#define GK_Z 44
+#define GK_X 45
 
-#define GK_Q 24
-#define GK_E 26
+#define GK_Q 16
+#define GK_E 18
 
-#define GK_SPACE 35 
+#define GK_SPACE 27 
 
 #endif  //_INPUT_H
